ab181e0c3a77df580a62477e75a8d4e4c5c101e0
gonz4216 <ethgon19@gmail.com>
2022-09-27 14:48:10 -0400

    Initial Commit

---
+++ code with chris/code_with_chrisApp.swift
@@ -0,0 +1,18 @@
+//
+//  code_with_chrisApp.swift
+//  code with chris
+//
+//  Created by Ethan Gonsalves on 2022-09-26.
+//
+
+import SwiftUI
+
+@main
+struct code_with_chrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
